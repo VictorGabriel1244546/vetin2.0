f4b2320af274aa009c3859f775406d2913e01055
Vitinngr <129069537+VictorGabriel1244546@users.noreply.github.com>
2023-06-30 15:48:59 -0400

    commit inicial
    ekinen

---
+++ pasta loja/script/games.js
@@ -0,0 +1,8 @@
+let gamesJson = [
+    {id:1, name:'Jogo Ps4', img:'../images/jg.png', price:22.29,  description:'Jogo Spider-Man para Ps4'},
+    {id:2, name:'Cadeira Gamer', img:'../images/cadei.png', price:500.00,  description:'Cadeira gamer '},
+    {id:3, name:'Camisa: Chapter II', img:'../images/cami.png', price:52.90, description:'Camisa Spider-Man'},
+    {id:4, name:'Controle', img:'../images/contro.png', price:60.99,  description:'Controle Spider-Man'},
+    {id:5, name:'Mouse', img:'../images/mause.png', price:46.99, description:'Mouse Spider-Man'},
+    {id:6, name:'Gabinete', img:'../images/gabin.png', price:600.99,  description:'Gabinete Spider-Man'},
+];
